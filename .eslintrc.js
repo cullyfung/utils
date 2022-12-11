ad55dbef29ad804dc4b8912e3bfd46f502c1a564
cullyfung <cullyfung@gmail.com>
2022-12-11 23:13:39 +0800

    chore: config prettier & eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,5 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true
-  },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: ['@typescript-eslint'],
-  rules: {}
-}
+const { defineConfig } = require('eslint-define-config')
+
+module.exports = defineConfig({
+  extends: ['@cullyfung']
+})
